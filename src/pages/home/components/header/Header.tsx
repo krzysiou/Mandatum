3af7e87b4537a4c3a21823ada8bbe6f6a45164bb
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2021-12-30 11:33:16 +0100

    changed home colors

--- src/pages/home/components/header/Header.tsx
+++ src/pages/home/components/header/Header.tsx
@@ -1,13 +1,13 @@
-import logo from './logo.png';
+//import logo from './logo.png';
 
 import "./Header.css"
 
 export function Header(): JSX.Element {
   return (
     <div>
-      <div className="logo">
+      {/* <div className="logo">
         <img src={logo} alt="mandatum logo" width="300" height="300"/>
-      </div>
+      </div> */}
       <div className="welcometext">
         <h1>Welcome to Mandatum</h1>
         <p>Connect with your friends through a website and share time by chatting</p>
