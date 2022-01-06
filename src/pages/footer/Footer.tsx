7b20aa0315d01455524c40182c2eb0846adf047a
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-01-06 15:08:37 +0100

    update footer

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer():JSX.Element {
     <div className='outer-container'>
       <div className="footer-container">
         <p className='footer-text'>Mandatum developed by krzysiou.</p>
-        <a className='footer-link' onClick={() => window.open('https://github.com/krzysiou', '_blank')}>Follow my github</a>
+        <a className='footer-link' onClick={() => window.open('https://github.com/krzysiou', '_blank')}>Follow my github here</a>
       </div>
     </div>
   )
