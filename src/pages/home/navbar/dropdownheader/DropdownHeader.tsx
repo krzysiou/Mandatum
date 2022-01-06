ecede52b85be10a43c995813857949f2c6a0edc3
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-01-06 11:20:39 +0100

    finished media querry for SM devices

--- src/pages/home/navbar/dropdownheader/DropdownHeader.tsx
+++ src/pages/home/navbar/dropdownheader/DropdownHeader.tsx
@@ -8,5 +8,5 @@ type Props = {
 }
 
 export function DropdownHeader({state, label, onPress, children}:Props): JSX.Element {
-  return <a className="menu-header" style={ {borderBottom: state ? '1px solid #1691a1' : '', padding: state ? '0 2em' : '' } } onClick={ onPress }>{ label } { state && children }</a>
+  return <a className="menu-header" style={ {borderBottom: state ? '1px solid #1691a1' : '', padding: state ? '0 1.5em' : '' } } onClick={ onPress }>{ label } { state && children }</a>
 }
