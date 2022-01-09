a6a2da3f5336eddf70eb7a47e3bde240917b1cfa
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-01-09 12:17:48 +0100

    test1

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,10 @@ import { Register } from "./pages/register/Register";
 
 import './App.css';
 
+//comment test1
+
+
+//comment test 2
 const rootElement = document.getElementById("root");
 render(
   <div className="App">
