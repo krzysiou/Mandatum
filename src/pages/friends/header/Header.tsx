f0b6a56d81835240d78de2e40d088867f18b9db1
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2022-01-23 22:13:14 +0100

    enchance friends tab

--- src/pages/friends/header/Header.tsx
+++ src/pages/friends/header/Header.tsx
@@ -1,19 +1,50 @@
 import './Header.css'
 import { SearchBar } from '../../../utilities/components/navbar/navbarcontents/searchbar/SearchBar'
 import { Button } from '../../../utilities/components/button/Button'
-import { addFriend, editProfile} from '../../../utilities/functions/globalFunctions'
+import { editProfile } from '../../../utilities/functions/globalFunctions'
+import { useState, useEffect } from 'react'
 import { FriendTab } from './friendtab/FriendTab'
-
-const User = {
-  username: "JohnDoe",
-}
+import { setCookie, getCookie } from '../../../utilities/functions/cookies'
+import axios from 'axios'
 
 export function Header(): JSX.Element {
+
+  const [message, setMessage] = useState('');
+  const [user, setUser] = useState({id: '', username: '', friends: [], pined: [], recent: []});
+
+  useEffect(()=>{
+    //get user data
+      axios.post('http://localhost:3001/users/get', {}, { headers: {'Authorization': `Bearer ${getCookie('accessToken')}`}})
+      .then(function (response) {
+        if(user != response.data.user){
+          setUser(response.data.user);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  async function addFriend() {
+    const friendToAdd = {
+      username: (document.getElementById('add-friend') as HTMLInputElement).value,
+    };
+    //add friend request with authorization
+    axios.post('http://localhost:3001/users/add', friendToAdd, { headers: {'Authorization': `Bearer ${getCookie('accessToken')}`}})
+    .then(function (response) {
+      setCookie('accessToken', response.data.accessToken, 1);
+      setMessage(response.data.message);
+    })
+    .catch(function (error) {
+      setMessage(error.response.data.error);
+    });
+  }
+
   return (
     <div className="friends-header">
       <div className='account-box box'>
         <div className='profile'>
-          <p className='username'>{User.username}</p>
+          <p className='username'>{user.username}</p>
         </div>
         <Button label="edit" width="4rem" onPress={editProfile()}/>
       </div>
@@ -24,8 +55,9 @@ export function Header(): JSX.Element {
         <div className='friend-box-container'>
           <FriendTab name="Thomas Evans"/>
         </div>
+        { message && <p className="message-handler"> { message } </p> }
         <SearchBar placeholder="add friend.." name="add-friend"/>
-        <Button label="add friend" width="8rem" onPress={addFriend('friend1')}/>
+        <Button label="add friend" width="8rem" onPress={addFriend}/>
       </div>
     </div>
   )
